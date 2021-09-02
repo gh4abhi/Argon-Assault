c47f656b04630e026e99a68c486c04b77a33ac16
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2021-09-02 23:58:13 +0530

    Added splash screen.

---
+++ Argon Assault Space Game/Assets/Scripts/NextToSplash.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextToSplash : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);  
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadLevelOne", 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadLevelOne()
+    {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(sceneIndex + 1);
+    }
+}
